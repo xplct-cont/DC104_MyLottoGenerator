45acb0929cd780088e76823f07ad1f7d9856f470
KennSecusana <kennsecusana@gmail.com>
2022-03-03 17:54:49 +0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="" src="./assets/pcso.png"> 
+  <HelloWorld msg=""/>
+</template>
+
+<script>
+import HelloWorld from './components/lotto-generator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 150px;
+  margin-left:50px;
+}
+
+img{
+  height: 50px;
+  width:50px;
+   margin-bottom: 150px;
+  margin-right:400px
+}
+
+</style>
---
+++ src/components/lotto-generator.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="container">    
+        <h1>{{title}}</h1>       
+        <div class="submit">
+         <button @click="generate(6, 1, 42);">Generate Numbers</button>      
+       </div>
+       <div id="numbers"></div>
+    </div>
+</template>
+    
+<script>    
+      export default {
+        data(){
+             return{
+              title:'My Lotto Generator',             
+          }        
+        },
+        methods: {
+         generate(size,lowest,highest){
+               var numbers = [];
+               for(var i = 0; i < size; i++) {var add = true;var randomNumber = Math.floor(Math.random() * highest) + 1;
+		for(var y = 0; y < highest; y++) {
+			if(numbers[y] == randomNumber) {
+				add = false;}}
+        if(add) {
+			numbers.push(randomNumber);
+		} else {
+			i--;
+		}
+	}
+
+var highestNumber = 0;
+	for(var m = 0; m < numbers.length; m++) {
+		for(var n = m + 1; n < numbers.length; n++) {
+			if(numbers[n] < numbers[m]) {
+				highestNumber = numbers[m];
+				numbers[m] = numbers[n];
+				numbers[n] = highestNumber;
+			}
+		}
+	} 
+	document.getElementById("numbers").innerHTML = numbers.join("    ");
+
+          }
+        }
+      }
+           
+</script>    
+<style lang="css">
+
+h1{
+    margin-top: -200px;
+    margin-bottom: 40px;
+    font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif ;
+}
+
+button{
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border-radius: 5px;
+    cursor:pointer;
+}
+
+.submit{
+  margin-bottom:10px;
+}
+
+button:hover{
+  background:#999;
+  color:#fff;
+}
+#numbers{
+  font-size:21px;
+  color:black;
+  font-family:'Times New Roman', Times, serif;
+}
+
+</style>
+
